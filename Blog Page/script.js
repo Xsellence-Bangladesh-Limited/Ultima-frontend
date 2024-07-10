219aa4f97bd1c896113b9b6cda7be04425e60d00
ashik <ashk11534@gmail.com>
2024-07-10 19:25:25 +0600

    Blog page done.

--- Blog Page/script.js
+++ Blog Page/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    console.log('Hello World.')
+    // Handling the all posts filter toggling (start)
+
+    let filterButtonClicked = false;
+
+    $('.all-blog-posts-filter-btn').click(function(){
+        filterButtonClicked = !filterButtonClicked;
+
+        if(filterButtonClicked){
+            $('.all-blog-posts-filter-menu-parent').fadeIn('slow');
+            $(".all-blog-posts-filter-btn .all-blog-posts-filter-label-arrow").removeClass('fa-chevron-down');
+            $(".all-blog-posts-filter-btn .all-blog-posts-filter-label-arrow").addClass('fa-chevron-up');
+        }
+
+        else{
+            $('.all-blog-posts-filter-menu-parent').fadeOut('slow');
+            $(".all-blog-posts-filter-btn .all-blog-posts-filter-label-arrow").addClass('fa-chevron-down');
+            $(".all-blog-posts-filter-btn .all-blog-posts-filter-label-arrow").removeClass('fa-chevron-up');
+        }
+        
+    });
+
+    // Handling the all posts filter toggling (end)
 })
