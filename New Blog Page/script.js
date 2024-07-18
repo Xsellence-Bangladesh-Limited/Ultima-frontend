374cb8769185ebce71ba031b5b2e24c3fa8ea09d
Ashik <ashk11534@gmail.com>
2024-07-18 19:12:46 +0600

    New blog page 90%.

---
+++ New Blog Page/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
